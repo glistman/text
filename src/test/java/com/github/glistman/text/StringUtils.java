cbc0fa03f29db0212820263c8735f45926c84e68
Lismtna <listman2390@gmail.com>
2018-12-19 00:59:43 -0500

    add trim function

---
+++ src/test/java/com/github/glistman/text/StringUtils.java
@@ -0,0 +1,9 @@
+package com.github.glistman.text;
+
+public class StringUtils {
+
+    public static String trim(String string) {
+        return (string == null) ? null : string.trim();
+    }
+
+}
